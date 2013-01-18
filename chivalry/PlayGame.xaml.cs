0ffe35d91aeccfc8df7cbf42d477df561b6d9c30
Nick Heiner <nth23@cornell.edu>
2013-01-17 23:46:15 -0500

    Drawing a board on the screen
    in the most embarassing way possible

--- chivalry/PlayGame.xaml.cs
+++ chivalry/PlayGame.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using chivalry.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using Windows.Foundation;
@@ -29,33 +30,35 @@ namespace chivalry
         }
 
         // adapted from http://code.msdn.microsoft.com/windowsapps/Reversi-XAMLC-sample-board-816140fa/sourcecode?fileId=69011&pathId=706708707
+        // This is shit code.
         void PlayGame_Loaded(object sender, RoutedEventArgs e)
         {
-            var rows = Enumerable.Range(0, 10);
-            var cols = Enumerable.Range(0, 10);
+            var rowsEnd = new int[] { 2, 8, 10, 12 };
+            var allRows = Enumerable.Concat(Enumerable.Concat(rowsEnd, Enumerable.Repeat(12, 8)), rowsEnd.Reverse());
 
-            foreach (var row in rows)
+            foreach (var row in allRows)
             {
-                boardGrid.RowDefinitions.Add(new RowDefinition());
+                boardGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(80) });
             }
-            foreach (var col in rows)
+            foreach (var col in Enumerable.Range(0, allRows.Max()))
             {
-                boardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                boardGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(80) });
             }
 
-            foreach (var row in rows)
+            foreach (var rowInfo in allRows.Select((colCount, index) => new { ColCount = colCount, Index = index }))
             {
-                foreach (var col in cols)
+                foreach (var col in Enumerable.Range(0, rowInfo.ColCount))
                 {
-                    var boardSpace = new BoardSpace();
-                    boardSpace.SetBinding(BoardSpace.SpaceStateProperty,
-                        new Binding { Path = new PropertyPath(String.Format("[{0},{1}]", row, col)) });
-                    boardSpace.SetBinding(BoardSpace.CommandProperty,
-                        new Binding { Path = new PropertyPath("MoveCommand") });
-                    boardSpace.CommandParameter = new Space(row, col);
-                    Grid.SetRow(boardSpace, row);
+                    var backgroundColorKey = rowInfo.Index % 2 == 0 ^ col % 2 == 0 ? "PrimaryTileColor" : "SecondaryTileColor";
+                    var boardSpace = new BoardSpace() { Background = (Brush)App.Current.Resources[backgroundColorKey] };
+                    //boardSpace.SetBinding(BoardSpace.SpaceStateProperty,
+                    //    new Binding { Path = new PropertyPath(String.Format("[{0},{1}]", row, col)) });
+                    //boardSpace.SetBinding(BoardSpace.CommandProperty,
+                    //    new Binding { Path = new PropertyPath("MoveCommand") });
+                    //boardSpace.CommandParameter = new Space(row, col);
+                    Grid.SetRow(boardSpace, rowInfo.Index);
                     Grid.SetColumn(boardSpace, col);
-                    boardGrid.Children.Add(boardSpace); 
+                    boardGrid.Children.Add(boardSpace);
                 }
             }
         }
