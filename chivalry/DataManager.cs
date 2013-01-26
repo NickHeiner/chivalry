a9770fd0962092e22eeb1c4713bea4bf9bbacb01
Nick Heiner <nth23@cornell.edu>
2013-01-25 19:14:28 -0500

    Adding starting positions

--- chivalry/DataManager.cs
+++ chivalry/DataManager.cs
@@ -26,7 +26,7 @@ namespace chivalry
                 againstScott.SetPieceLocation(5, 8, BoardSpaceState.OpponentPieceTall);
 
                 user.Games.Add(againstScott);
-                user.Games.Add(new Game() { RecepientPlayerName = "Dad" });
+                user.Games.Add(withStartingPieces(new Game() { RecepientPlayerName = "Dad" }));
 
                 return user;
             }
@@ -47,8 +47,39 @@ namespace chivalry
                 InitiatingPlayerName = initiatingPlayerName,
                 InitiatingPlayerEmail = initiatingPlayerEmail,
                 RecepientPlayerName = againstUserName, 
-                RecepientPlayerEmail = againstUserEmail
+                RecepientPlayerEmail = againstUserEmail,
             });
+        }
+
+        private Game withStartingPieces(Game game)
+        {
+            //  This isn't as declarative as I'd like, but it's tough in a language
+            //  without object literals
+            fillTeam(game, 5, 1, BoardSpaceState.FriendlyPieceShort, BoardSpaceState.FriendlyPieceTall);
+            fillTeam(game, 10, -1, BoardSpaceState.OpponentPieceShort, BoardSpaceState.OpponentPieceTall);
+
+            return game;
+        }
+
+        private void fillTeam(Game game, int startRow, int nextRowOffset, BoardSpaceState shortPiece, BoardSpaceState tallPiece)
+        {
+            int nextRow = startRow + nextRowOffset;
+
+            game.SetPieceLocation(startRow, 2, tallPiece);
+            game.SetPieceLocation(nextRow, 3, tallPiece);
+            game.SetPieceLocation(startRow, 9, tallPiece);
+            game.SetPieceLocation(nextRow, 8, tallPiece);
+
+            fillRow(game, shortPiece, startRow, 3, 8);
+            fillRow(game, shortPiece, nextRow, 4, 7);
+        }
+
+        private void fillRow(Game game, BoardSpaceState toFill, int row, int lowerColBoundInclusive, int upperColBoundInclusive)
+        {
+            foreach (int col in Enumerable.Range(lowerColBoundInclusive, upperColBoundInclusive - lowerColBoundInclusive + 1))
+            {
+                game.SetPieceLocation(row, col, toFill);
+            }
         }
     }
 }
