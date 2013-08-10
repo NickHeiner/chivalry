c7e3b3a88f8a66c3698bdf1b301689b63f611d4c
Nick Heiner <nth23@cornell.edu>
2013-08-10 17:25:35 -0400

    adding an f# lib for funsies
    also trying to extract api key

---
+++ camelot/PortableLibrary1.fs
@@ -0,0 +1,10 @@
+﻿namespace camelot
+
+module camelot =
+    let IsPrime n =
+            if n < 2L then
+                false
+            else
+                seq { 2L .. n - 1L}
+                |> Seq.exists(fun x -> n % x = 0L)
+                |> not
--- chivalry/App.xaml.cs
+++ chivalry/App.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using camelot;
 
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
 
@@ -26,8 +27,19 @@ namespace chivalry
     /// </summary>
     sealed partial class App : Application
     {
-        public static MobileServiceClient MobileService = 
-            new MobileServiceClient("https://chivalry.azure-mobile.net/", "QpzUUCwKqDGWvCSSggLommMKMzkaca83");
+
+        public static MobileServiceClient MobileService;
+
+        async static App()
+        {
+            var file = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///api-key.txt"));
+            var read = await file.OpenReadAsync();
+            var apiKey = read.ToString();
+
+            MobileService = new MobileServiceClient("https://chivalry.azure-mobile.net/", apiKey);
+
+            camelot.camelot.IsPrime(34);
+        }
 
         public static PushNotificationChannel CurrentChannel { get; private set; }
 
